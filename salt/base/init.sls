b6d083977d4545f5ae70b7d100e99a3c1468e6d8
Olivier Mauras <olivier@mauras.ch>
2018-02-09 16:32:45 +0100

    Added it in installer https://git.mauras.ch/voidlinux/luks-lvm-install/commit/078bad7be5d8b0f350209edc37a07426b7ddf471

--- salt/base/init.sls
+++ salt/base/init.sls
@@ -3,6 +3,3 @@ include:
   - .services
   - .logrotate
   - .sudo
-{% if 'Intel' in grains['cpu_model'] %}
-  - .intel-microcode
-{% endif %}
--- salt/base/scripts/kernel_reconfigure.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-KERN_LIST=`ls /boot/vmlinuz-* | cut -f 2 -d - | cut -f -2 -d . | sort | uniq`
-for KERN_VER in ${KERN_LIST}; do
-  xbps-reconfigure -f linux${KERN_VER}
-done
